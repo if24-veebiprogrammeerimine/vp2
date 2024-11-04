7507065c72a1a6815f71a042acb68b6bd479b424
Andrus Rinde <andrusrinde@users.noreply.github.com>
2024-11-04 13:32:38 +0200

    Kasutajate loomine ja sisse/välja logimine tehtud

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const mysql = require("mysql2");
 const multer = require("multer");
 //fotomanipulatsiooniks
 const sharp = require("sharp");
+//paroolide krüpteerimiseks
+const bcrypt = require("bcrypt");
+//sessioonihaldur
+const session = require("express-session");
 
 
 const app = express();
@@ -23,6 +27,9 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended: true}));
 //seadistame fotode üleslaadimiseks vahevara (middleware), mis määrab kataloogi, kuhu laetakse
 const upload = multer({dest: "./public/gallery/orig"});
+//sessioonihaldur
+app.use(session({secret: "minuAbsoluutseltSalajaneVõti", saveUninitialized: true, resave: true}));
+let mySession;
 
 //loon andmebaasiühenduse
 const connInga = mysql.createConnection({
@@ -42,6 +49,107 @@ app.get("/", (req, res)=>{
 	//res.send("Express läks käima!");
 	//console.log(dbInfo.configData.host);
 	res.render("index");
+});
+
+app.post("/", (req, res)=>{
+	let notice = null;
+	if(!req.body.emailInput || !req.body.passwordInput){
+		console.log("Sisselogimise andmed pole täielikud!");
+		notice = "Sisselogimise andmeid on puudu!";
+		res.render("index", {notice: notice});
+	}
+	else {
+		let sqlReq = "SELECT id,password FROM vp2users WHERE email = ?";
+		conn.execute(sqlReq, [req.body.emailInput], (err, result)=>{
+			if(err){
+				notice = "Tehnilise vea tõttu ei saa sisse logida!";
+				console.log(err);
+				res.render("index", {notice: notice});
+			}
+			else {
+				if(result[0] != null){
+					//kontrollime, kas sisselogimisel sisestatud paroolist saaks sellise räsi nagu andmebaasis
+					bcrypt.compare(req.body.passwordInput, result[0].password, (err, compareresult)=>{
+						if(err){
+							notice = "Tehnilise vea tõttu andmete kontrollimisel ei saa sisse logida!";
+							console.log(err);
+							res.render("index", {notice: notice});
+						}
+						else {
+							//kui võrdlustulemus  on positiivne
+							if(compareresult){
+								notice = "Oledki sisseloginud!";
+								//võtame sessiooni kasutusele
+								mySession = req.session;
+								mySession.userId = result[0].id;
+								//res.render("index", {notice: notice});
+								res.redirect("/home");
+							}
+							else {
+								notice = "Kasutajatunnus ja/või parool oli vale!";
+								res.render("index", {notice: notice});
+							}
+						}
+					});
+				}
+				else {
+					notice = "Kasutajatunnus või parool oli vale!";
+					res.render("index", {notice: notice});
+				}
+			}
+		});
+	}
+	//res.render("index");
+});
+
+app.get("/home", checkLogin, (req, res)=>{
+	console.log("Sisse on loginud kasutaja: " + mySession.userId);
+	res.render("home");
+});
+
+app.get("/signup", (req, res)=>{
+	res.render("signup");
+});
+
+app.post("/signup", (req, res)=>{
+	let notice = "Ootan andmeid";
+	if(!req.body.firstNameInput || !req.body.lastNameInput || !req.body.birthDateInput || !req.body.genderInput || !req.body.emailInput || req.body.passwordInput.length < 8 || req.body.passwordInput !== req.body.confirmPasswordInput){
+		console.log("Andmeid puudu või paroolid ei klapi!");
+		notice = "Andmeid on puudu või paroolid ei kattu!";
+		res.render("signup", {notice: notice});
+	}
+	else {
+		notice = "Andmed korras!";
+		bcrypt.genSalt(10, (err, salt)=>{
+			if(err){
+				notice = "Tehniline viga, kasutajat ei loodud.";
+				res.render("signup", {notice: notice});
+			}
+			else {
+				bcrypt.hash(req.body.passwordInput, salt, (err, pwdHash)=>{
+					if(err){
+						notice = "Tehniline viga parooli krüpteerimisel, kasutajat ei loodud.";
+						res.render("signup", {notice: notice});
+					}
+					else {
+						let sqlReq = "INSERT INTO vp2users (first_name, last_name, birth_date, gender, email, password) VALUES(?,?,?,?,?,?)";
+						conn.execute(sqlReq, [req.body.firstNameInput, req.body.lastNameInput, req.body.birthDateInput, req.body.genderInput, req.body.emailInput, pwdHash], (err, result)=>{
+							if(err){
+								notice = "Tehniline viga andmebaasi kirjutamisel, kasutajat ei loodud.";
+								res.render("signup", {notice: notice});
+							}
+							else {
+								notice = "Kasutaja " + req.body.emailInput + " edukalt loodud!";
+								res.render("signup", {notice: notice});
+							}
+						});
+					}
+				});
+			}
+		});
+		//res.render("signup", {notice: notice});
+	}
+	//res.render("signup");
 });
 
 app.get("/timenow", (req, res)=>{
@@ -197,5 +305,21 @@ app.get("/gallery", (req, res)=>{
 	});
 	//res.render("gallery");
 });
+
+function checkLogin(req, res, next){
+	if(mySession != null){
+		if(mySession.userId){
+			console.log("Login ok!");
+			next();
+		}
+		else {
+			console.log("Login not detected!");
+			res.redirect("/");
+		}
+	}
+	else {
+		res.redirect("/");
+	}
+}
 
 app.listen(5200);
